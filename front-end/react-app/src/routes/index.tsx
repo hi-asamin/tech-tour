a05bb2f1fda62c85bc67aa6ce575b9678cb450e5
marc-town <asamin.itconsultant@gmail.com>
2020-11-17 15:11:01 +0900

    #8 split routes
    can not desplay 404 page

--- front-end/react-app/src/routes/index.tsx
+++ front-end/react-app/src/routes/index.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Index } from 'ui/pages';
 import { CurriculumRoute } from 'routes/curriculum/curriculum-route';
 import { DummyRoute } from 'routes/dummy-route';
-import { NotFound } from 'ui/pages/error/not-found';
 
 export const AppRoute = () => {
   return (
     <Router>
-      <Switch>
-        <React.Fragment>
-          <Route exact path='/' component={Index} />
-          <CurriculumRoute />
-          <DummyRoute />
-        </React.Fragment>
-        <Route component={NotFound} />
-      </Switch>
+      <Route exact path='/' component={Index} />
+      <CurriculumRoute />
+      <DummyRoute />
     </Router>
   );
 }
