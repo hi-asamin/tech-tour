8804a2c39b53dd054cb068f41c3d3cb5b384abad
marc-town <asamin.itconsultant@gmail.com>
2020-12-27 21:26:27 +0900

    update field of chapters

--- front-end/react-app/src/ui/components/pages/books/chapters.tsx
+++ front-end/react-app/src/ui/components/pages/books/chapters.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Controller, UseFormMethods } from "react-hook-form";
+import { Controller, UseFormMethods, ArrayField } from "react-hook-form";
 
 import { BookRequest, Chapter } from 'domain/api/models/book';
 
@@ -9,56 +9,40 @@ import InputLabel from '@material-ui/core/InputLabel';
 import TextField from '@material-ui/core/TextField';
 
 export interface ChaptersProps {
-  chapters: Chapter[],
-  addChapters: () => void,
-  removeChapters: (chapter: Chapter) => () => void,
-  formHooks: UseFormMethods<BookRequest>,
-}
-
-interface ChapterFieldsProps {
-  chapter: Chapter,
-  index: number,
-  removeChapters: (chapter: Chapter) => () => void,
+  chapters: Partial<ArrayField<Record<string, any>, "id">>,
+  remove: (index: number) => () => void,
   formHooks: UseFormMethods<BookRequest>,
 }
 
 export const Chapters = (props: ChaptersProps) => {
-  const { chapters, addChapters, removeChapters, formHooks } = props;
+  const { chapters, remove,  formHooks } = props;
   return (
     <>
       <FormGroup>
         <InputLabel>目次</InputLabel>
-        <Button variant="contained" color="primary" onClick={addChapters}>追加</Button>
-        {chapters.map((chapter: Chapter, index: number) =>
-          <ChapterFields chapter={chapter} index={index} removeChapters={removeChapters} formHooks={formHooks} />
-        )}
+        {chapters.map((_: Chapter, index: number) => {
+          return (
+            <>
+              <Controller
+                as={TextField}
+                label={`目次 ${index+1}`}
+                name={`chapters[${index}].chpater`}
+                ref={formHooks.register}
+                control={formHooks.control}
+                defaultValue=''
+                margin="normal"
+                fullWidth
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant='outlined'
+                placeholder='目次を入力してください'
+              />
+              <Button variant="contained" color="secondary" onClick={remove(index)}>削除</Button>
+            </>
+          )
+        })}
       </FormGroup>
     </>
   )
-};
-
-const ChapterFields = (props: ChapterFieldsProps) => {
-  const { chapter, index, removeChapters, formHooks } = props;
-  const fieldPrefix = `chapters[${index}].chpater`;
-  return (
-    <>
-      <InputLabel>目次: {index + 1}</InputLabel>
-      <Controller
-        as={TextField}
-        required
-        label='目次'
-        name={fieldPrefix}
-        ref={formHooks.register}
-        control={formHooks.control}
-        margin="normal"
-        fullWidth
-        InputLabelProps={{
-          shrink: true,
-        }}
-        variant='outlined'
-        placeholder='目次を入力してください'
-      />
-      <Button variant="contained" color="secondary" style={{marginRight: '10px'}} onClick={removeChapters(chapter)}>削除</Button>
-    </>
-  );
 };
--- front-end/react-app/src/ui/components/pages/books/create.tsx
+++ front-end/react-app/src/ui/components/pages/books/create.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useForm, Controller } from "react-hook-form";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 import { useHistory } from 'react-router-dom';
 
 import { Chapters } from 'ui/components/pages/books/chapters';
@@ -15,23 +15,24 @@ import Typography from '@material-ui/core/Typography';
 
 export const CreatePage = () => {
   const history = useHistory();
-  const [chapters, setChapters] = useState<Chapter[]>([]);
   const [age, setAge] = useState('');
   const formHooks = useForm<BookRequest>();
+  const { fields, append, remove } = useFieldArray({
+    control: formHooks.control,
+    name: "chapters"
+  });
   const onBackPage = () => {
     history.push('/book');
   }
   const onSubmit = (data: BookRequest) => {
     alert(JSON.stringify(data));
   }
-  const addChapters = () => {
-    const newChapters: Chapter[] = [...chapters];
-    newChapters.push({ chapter: '' });
-    setChapters(newChapters);
+  const addChapter = () => {
+    append({ chapter: '' })
   };
 
-  const removeChapters = (chapter: Chapter) => () => {
-    setChapters((current: Chapter[]) => [...current.filter(i => i !== chapter)]);
+  const removeChapter = (index: number) => () => {
+    remove(index);
   };
 
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
@@ -41,93 +42,99 @@ export const CreatePage = () => {
     <React.Fragment>
       <Typography gutterBottom variant="h3" component="h2">書籍登録</Typography>
       <form onSubmit={formHooks.handleSubmit(onSubmit)} encType='multipart/form-data'>
-        <div style={{width: 600, margin: 'auto', display: 'flex', flexDirection: 'column'}} >
+        <Controller
+          as={TextField}
+          required
+          label='タイトル'
+          name='title'
+          ref={formHooks.register}
+          control={formHooks.control}
+          margin="normal"
+          fullWidth
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant='outlined'
+          placeholder='本のタイトルを入力してください'
+        />
+        <Controller
+          as={TextField}
+          label='著者'
+          name='author'
+          ref={formHooks.register}
+          control={formHooks.control}
+          defaultValue=''
+          margin="normal"
+          fullWidth
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant='outlined'
+          placeholder='本の著者を入力してください'
+        />
+        {/* <input
+          accept="image/*"
+          name='image'
+          ref={formHooks.register}
+          id="contained-button-file"
+          multiple
+          type="file"
+        /> */}
+        <Controller
+          as={TextField}
+          label='画像'
+          name='image'
+          ref={formHooks.register}
+          control={formHooks.control}
+          defaultValue=''
+          margin="normal"
+          fullWidth
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant='outlined'
+          placeholder='画像を入力してください'
+        />
+        <FormControl variant="outlined">
+          <InputLabel shrink>ジャンル</InputLabel>
           <Controller
-            as={TextField}
-            required
-            label='タイトル'
-            name='title'
+            as={Select}
+            name='genre_id'
             ref={formHooks.register}
             control={formHooks.control}
-            margin="normal"
-            fullWidth
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant='outlined'
-            placeholder='本のタイトルを入力してください'
-          />
-          <Controller
-            as={TextField}
-            required
-            label='著者'
-            name='author'
-            ref={formHooks.register}
-            control={formHooks.control}
-            margin="normal"
-            fullWidth
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant='outlined'
-            placeholder='本の著者を入力してください'
-          />
-          <input
-            accept="image/*"
-            name='image'
-            ref={formHooks.register}
-            id="contained-button-file"
-            multiple
-            type="file"
-          />
-          <FormControl variant="outlined">
-            <InputLabel shrink>ジャンル</InputLabel>
-            <Controller
-              as={Select}
-              name='genre_id'
-              ref={formHooks.register}
-              control={formHooks.control}
-              value={age}
-              onChange={handleChange}
-              label="Age"
-            >
-              <MenuItem>
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={1}>政治・経済</MenuItem>
-              <MenuItem value={1}>雑学</MenuItem>
-              <MenuItem value={1}>ゴミ</MenuItem>
-            </Controller>
-          </FormControl>
-          <Chapters
-            chapters={chapters}
-            addChapters={addChapters}
-            removeChapters={removeChapters}
-            formHooks={formHooks}
-          />
-          <Controller
-            as={TextField}
-            label="メモ"
-            name='memo'
-            ref={formHooks.register}
-            control={formHooks.control}
-            multiline
-            rows={4}
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
-            placeholder='4行までメモできます'
-          />
-          <Button variant="contained" onClick={onBackPage}>
-            戻る
-          </Button>
-          <Button type='submit' variant="contained" size='medium' color="primary">
-            登録
-          </Button>
-        </div>
+            value={age}
+            onChange={handleChange}
+            label="Age"
+          >
+            <MenuItem>
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={1}>政治・経済</MenuItem>
+            <MenuItem value={1}>雑学</MenuItem>
+            <MenuItem value={1}>ゴミ</MenuItem>
+          </Controller>
+        </FormControl>
+        <Chapters　chapters={fields} remove={removeChapter} formHooks={formHooks} />
+        <Button variant="contained" color="primary" onClick={addChapter}>追加</Button>
+        <Controller
+          as={TextField}
+          label="メモ"
+          name='memo'
+          ref={formHooks.register}
+          control={formHooks.control}
+          defaultValue=''
+          multiline
+          rows={4}
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+          placeholder='4行までメモできます'
+        />
+        <Button variant="contained" onClick={onBackPage}>戻る</Button>
+        <Button type='submit' variant="contained" size='medium' color="primary">登録</Button>
       </form>
     </React.Fragment>
   )
